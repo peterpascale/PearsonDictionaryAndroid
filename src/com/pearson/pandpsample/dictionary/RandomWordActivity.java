4015eb7cbc98d4917674ab9e417e89c67189215e
Peter Pascale <peter.pascale@gmail.com>
2012-05-27 22:50:19 -0500

    Initial check-in

---
+++ src/com/pearson/pandpsample/dictionary/RandomWordActivity.java
@@ -0,0 +1,13 @@
+package com.pearson.pandpsample.dictionary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RandomWordActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
